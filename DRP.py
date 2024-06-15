645bc6cfe8e96ff69b0379f0530e0be0dad0c9c6
Esther <151816213+SterTheStar@users.noreply.github.com>
2024-06-15 12:10:14 -0300

    Update DRP.py

--- DRP.py
+++ DRP.py
@@ -31,7 +31,7 @@ import sys
 import json
 import urllib.request
 import time
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QComboBox, QMessageBox, QTabWidget, QWidget, QVBoxLayout, QHBoxLayout, QFileDialog, QProgressBar, QLineEdit, QTextEdit, QDialog, QScrollArea, QAction
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QComboBox, QMessageBox, QTabWidget, QWidget, QVBoxLayout, QHBoxLayout, QFileDialog, QProgressBar, QLineEdit, QTextEdit, QDialog, QScrollArea, QAction, QSpinBox  # Add QSpinBox import here
 from PyQt5.QtGui import QFont, QIcon
 from PyQt5.QtCore import Qt, QThread, pyqtSignal, QTimer, QUrl, QSettings, QDir
 from PyQt5.QtGui import QDesktopServices
@@ -40,8 +40,11 @@ from qtmodern.windows import ModernWindow
 from packaging.version import Version
 
 class DownloadThread(QThread):
+    # Sinais para atualizar a interface de usuário
     update_progress = pyqtSignal(int)
     update_speed = pyqtSignal(float, QProgressBar)
+    update_buttons = pyqtSignal(bool, bool)  # Para atualizar os estados dos botões
+    cancel_download = pyqtSignal()  # Novo sinal para cancelamento de download
 
     def __init__(self, url, save_path, progress_bar, parent=None):
         super().__init__(parent)
@@ -50,6 +53,7 @@ class DownloadThread(QThread):
         self.progress_bar = progress_bar
         self.start_time = None
         self.downloaded_bytes = 0
+        self.paused = False  # Flag para controle de pausa
         self._terminate = False
 
     def run(self):
@@ -57,11 +61,19 @@ class DownloadThread(QThread):
             self.start_time = time.time()
             self.request, _ = urllib.request.urlretrieve(self.url, self.save_path, reporthook=self.report_hook)
         except Exception as e:
-            print(f"Download error: {e}")
+            if self._terminate:
+                self.update_progress_cancel()
+            else:
+                print(f"Download error: {e}")
 
     def report_hook(self, blocknum, blocksize, totalsize):
         if self._terminate:
             raise Exception("Download cancelled by user.")
+
+        if self.paused:
+            while self.paused:
+                time.sleep(1)
+
         if totalsize > 0:
             self.downloaded_bytes += blocksize
             progress = blocknum * blocksize * 100 / totalsize
@@ -71,9 +83,22 @@ class DownloadThread(QThread):
             download_speed = (self.downloaded_bytes / 1024 / 1024) / elapsed_time if elapsed_time > 0 else 0
             self.update_speed.emit(download_speed, self.progress_bar)
 
+    def toggle_pause(self):
+        self.paused = not self.paused
+        self.update_buttons.emit(not self.paused, self.paused)
+
     def terminate(self):
         self._terminate = True
         urllib.request.urlcleanup()
+
+    def update_progress_cancel(self):
+        self.progress_bar.setValue(0)
+        self.progress_bar.setStyleSheet("QProgressBar::chunk { background-color: red; }")
+        self.progress_bar.setAlignment(Qt.AlignCenter)  # Centralizar texto
+
+    def restore_progress_bar(self):
+        self.progress_bar.setStyleSheet("")  # Remover estilo para voltar ao padrão
+        self.progress_bar.setAlignment(Qt.AlignLeft)  # Alinhar texto à esquerda padrão
 
 class LicenseDialog(QDialog):
     def __init__(self, license_text, parent=None):
@@ -105,9 +130,15 @@ class MainWindow(QMainWindow):
         self.settings_file = 'config.json'  # Nome do arquivo de configuração JSON
         self.settings = QSettings("DRP Studios", "DRP")
 
+        self.settings_file = 'config.json'  # Nome do arquivo de configuração JSON
+        self.download_path = ''  # Inicialização da variável para o caminho de download
+        self.download_path_edit = QLineEdit() 
+
+        self.limit_speed_spinbox = QSpinBox()
+
         self.load_settings()
 
-        self.setWindowTitle('DRP 1.0 - Pulse')
+        self.setWindowTitle('DRP 1.1 - Pride')
         self.setGeometry(200, 200, 600, 400)
 
         self.tab_widget = QTabWidget(self)
@@ -125,6 +156,10 @@ class MainWindow(QMainWindow):
 
         self.download_thread = None
 
+        self.setStyleSheet('')
+
+        self.load_settings_theme()
+
     def check_update_and_display_changelog(self):
         url = 'https://raw.githubusercontent.com/SterTheStar/DRPBR/main/changelogs.json'
 
@@ -135,7 +170,7 @@ class MainWindow(QMainWindow):
 
                 latest_version = max(changelogs.keys(), key=lambda v: Version(v))
 
-                if Version('1.0') < Version(latest_version):
+                if Version('1.1') < Version(latest_version):
                     # Build the changelog text
                     changelog_text = f"Version: {changelogs[latest_version]['version']}\n"
                     changelog_text += f"Date: {changelogs[latest_version]['date']}\n\n"
@@ -144,7 +179,7 @@ class MainWindow(QMainWindow):
                         changelog_text += f"- {change}\n"
 
                     # Check if update is needed
-                    if Version('1.0') < Version(latest_version):
+                    if Version('1.1') < Version(latest_version):
                         # Ask user if they want to update and show changelog
                         reply = QMessageBox.question(self, 'Update Available',
                                                      f"A new version ({latest_version}) is available.\n\n"
@@ -183,15 +218,22 @@ class MainWindow(QMainWindow):
             with open(self.settings_file, 'r') as f:
                 json_data = json.load(f)
                 self.download_path = json_data.get('download_path', '')
+                self.download_speed = json_data.get('download_speed', 0)
         except FileNotFoundError:
             pass
         except Exception as e:
             print(f"Failed to load settings from JSON: {e}")
 
+        # Update QLineEdit only if it is initialized
+        if self.download_path_edit:
+            self.download_path_edit.setText(self.download_path)  # Check before setting text
+
+        self.limit_speed_spinbox.setValue(int(self.download_speed))
+
     def save_settings(self):
         self.settings.beginGroup("DownloadSettings")
         self.settings.setValue("download_path", self.download_path)
-        self.settings.setValue("download_speed", self.download_speed)
+        self.settings.setValue("download_speed", self.limit_speed_spinbox.value())  # Salvando o valor do spinbox
         self.settings.endGroup()
 
         self.save_settings_to_json()
@@ -199,7 +241,7 @@ class MainWindow(QMainWindow):
     def save_settings_to_json(self):
         try:
             with open(self.settings_file, 'w') as f:
-                json.dump({'download_path': self.download_path}, f, indent=4)
+                json.dump({'download_path': self.download_path, 'download_speed': self.limit_speed_spinbox.value()}, f, indent=4)
         except Exception as e:
             print(f"Failed to save settings to JSON: {e}")
 
@@ -209,7 +251,7 @@ class MainWindow(QMainWindow):
 
         title_label = QLabel(f'DRP {tab_name}', tab)
         title_label.setAlignment(Qt.AlignCenter)
-        title_label.setFont(QFont('Arial', 14))
+        title_label.setFont(QFont('Courier New', 25))
         layout.addWidget(title_label)
 
         combo_layout1 = QHBoxLayout()
@@ -248,9 +290,15 @@ class MainWindow(QMainWindow):
         btn_download = QPushButton('Download', tab)
         button_layout.addWidget(btn_download)
 
+        btn_pause_resume = QPushButton('Pause', tab)  # Adicionando botão de pausa
+        button_layout.addWidget(btn_pause_resume)
+        btn_pause_resume.setEnabled(False)
+
         btn_cancel = QPushButton('Cancel', tab)
         btn_cancel.setEnabled(False)
         button_layout.addWidget(btn_cancel)
+
+        btn_cancel.clicked.connect(self.cancel_download)
 
         layout.addLayout(button_layout)  # Adiciona o layout dos botões ao layout principal
 
@@ -271,7 +319,7 @@ class MainWindow(QMainWindow):
 
         combo_windows.currentIndexChanged.connect(lambda: self.on_windows_version_changed(combo_windows, combo_arch, combo_version, download_data))
         combo_arch.currentIndexChanged.connect(lambda: self.on_architecture_changed(combo_windows, combo_arch, combo_version, download_data))
-        btn_download.clicked.connect(lambda: self.download_iso(combo_windows, combo_arch, combo_version, download_data, progress_bar, btn_cancel))
+        btn_download.clicked.connect(lambda: self.download_iso(combo_windows, combo_arch, combo_version, download_data, progress_bar, btn_cancel, btn_pause_resume))
         btn_cancel.clicked.connect(self.cancel_download)
 
         # Novo layout para a configuração de download
@@ -282,6 +330,7 @@ class MainWindow(QMainWindow):
         layout.addLayout(button_layout2)
 
         btn_open_browser.clicked.connect(lambda: self.open_in_browser(combo_windows, combo_arch, combo_version, download_data))
+        btn_pause_resume.clicked.connect(lambda: self.pause_resume_download(btn_pause_resume, btn_cancel))
 
         return tab
 
@@ -295,13 +344,41 @@ class MainWindow(QMainWindow):
         settings_layout.addWidget(download_path_label)
 
         self.download_path_edit = QLineEdit()
-        self.download_path_edit.setText(self.download_path)
         settings_layout.addWidget(self.download_path_edit)
 
         btn_choose_path = QPushButton('🔎', tab)
         settings_layout.addWidget(btn_choose_path)
 
         layout.addLayout(settings_layout)
+
+        # Adicionando a opção de limite de velocidade de download
+        limit_speed_layout = QHBoxLayout()
+
+        limit_speed_label = QLabel('Download Speed Limit (MB/s): (In Working)', tab)
+        limit_speed_layout.addWidget(limit_speed_label)
+
+        self.limit_speed_spinbox = QSpinBox(tab)
+        self.limit_speed_spinbox.setMinimum(0)  # Definindo valor mínimo como 0 MB/s
+        self.limit_speed_spinbox.setMaximum(100)  # Definindo valor máximo como 100 MB/s
+        limit_speed_layout.addWidget(self.limit_speed_spinbox)
+
+        layout.addLayout(limit_speed_layout)
+
+        # Dentro da função create_settings_tab()
+
+        theme_layout = QHBoxLayout()
+        layout.addLayout(theme_layout)
+
+        # Dropdown para escolher o estilo da janela principal
+        style_label = QLabel('Select Main Window Style:', tab)
+        theme_layout.addWidget(style_label)
+
+        self.style_dropdown = QComboBox(tab)
+        self.style_dropdown.addItems(['Default Style', 'Dark Style', 'Pride Style'])  # Exemplo de opções
+        theme_layout.addWidget(self.style_dropdown)
+
+        # Conectar sinal para mudar o estilo da janela principal
+        self.style_dropdown.currentIndexChanged.connect(self.change_main_window_style)
 
         # Adicionando espaçamento flexível para empurrar os botões para a parte inferior
         layout.addStretch()
@@ -326,8 +403,7 @@ class MainWindow(QMainWindow):
         # Adicionando o botão "Save Settings" em uma linha separada abaixo dos botões acima
         layout.addSpacing(0)  # Espaçamento entre as linhas
         btn_save_settings = QPushButton('Save Settings', tab)
-        btn_save_settings.setMinimumWidth(580)  # Definindo largura mínima
-        layout.addWidget(btn_save_settings, alignment=Qt.AlignLeft)  # Alinhando à esquerda
+        layout.addWidget(btn_save_settings, alignment=Qt.AlignRight)  # Alinhando à esquerda
 
         # Conectando os sinais aos slots (eventos)
         btn_source_code.clicked.connect(self.open_source_code)
@@ -336,7 +412,55 @@ class MainWindow(QMainWindow):
         btn_save_settings.clicked.connect(self.save_settings)
         btn_license.clicked.connect(self.show_license)
 
+        # Carregando configurações
+        self.load_settings()
+
         return tab
+    
+    def change_main_window_style(self):
+        selected_style = self.style_dropdown.currentText()
+        file_path = ''
+
+        if selected_style == 'Dark Style':
+            file_path = 'data/style_dark.css'
+        elif selected_style == 'Pride Style':
+            file_path = 'data/style_trans.css'
+        else:
+            file_path = 'data/style.css'
+
+        try:
+            with open(file_path, 'r', encoding='utf-8') as f:
+                style = f.read()
+                self.setStyleSheet(style)
+                self.settings.setValue("main_window_style", selected_style)
+        except FileNotFoundError:
+            print(f"Error: Could not find the style file '{file_path}'")
+            # Handle the error as per your application's requirements
+        except Exception as e:
+            print(f"Error: {e}")
+            # Handle other potential exceptions
+
+    def save_style_settings(self, selected_style):
+        self.settings.setValue("main_window_style", selected_style)
+        self.settings.sync()  # Garante que as alterações sejam salvas imediatamente
+
+    def load_settings_theme(self):
+        saved_style = self.settings.value("main_window_style")
+
+        if saved_style is not None and self.style_dropdown is not None:
+            index = self.style_dropdown.findText(saved_style)
+            if index != -1:
+                self.style_dropdown.setCurrentIndex(index)
+            else:
+                self.apply_default_style()
+        else:
+            self.apply_default_style()
+
+
+    def apply_default_style(self):
+        # Carrega o estilo padrão
+        with open(self.default_style_path, 'r', encoding='utf-8') as f:
+            self.setStyleSheet(f.read())
     
     def show_license(self):
         try:
@@ -384,10 +508,16 @@ class MainWindow(QMainWindow):
                 if version['architecture'] == selected_architecture:
                     combo_version.addItems(version['versions'].keys())
 
-    def download_iso(self, combo_windows, combo_arch, combo_version, download_data, progress_bar, btn_cancel):
+    def cancel_download(self):
+        if self.download_thread and self.download_thread.isRunning():
+            self.download_thread.terminate()
+            self.download_thread.wait()
+
+    def download_iso(self, combo_windows, combo_arch, combo_version, download_data, progress_bar, btn_cancel, btn_pause_resume):
         selected_index = combo_windows.currentIndex()
         selected_architecture = combo_arch.currentText()
         selected_version = combo_version.currentText()
+        btn_pause_resume.setEnabled(True)
 
         btn_download = self.sender()
         btn_download.setEnabled(False)
@@ -399,16 +529,43 @@ class MainWindow(QMainWindow):
                 if version['architecture'] == selected_architecture:
                     if selected_version in version['versions']:
                         download_url = version['versions'][selected_version]
-                        save_path = f"{self.download_path}/{selected_version}.iso"
-                        speed_limit = self.download_speed
+                       # Construindo o caminho e nome do arquivo
+                        filename = f"DRP_{combo_windows.currentText()}_{selected_architecture}_{selected_version}.iso"
+                        save_path = f"{self.download_path}/{filename}"
+                        speed_limit = self.limit_speed_spinbox.value() * 1024 * 1024  # Convertendo para bytes/s
+
+                        # Cancelar download anterior, se houver
+                        if self.download_thread and self.download_thread.isRunning():
+                            self.cancel_download()
+
+                        # Criar novo thread de download
                         self.download_thread = DownloadThread(download_url, save_path, progress_bar)
                         self.download_thread.update_progress.connect(progress_bar.setValue)
                         self.download_thread.update_speed.connect(self.update_download_speed)
+                        self.download_thread.cancel_download.connect(self.update_progress_cancel)  # Conexão para atualizar quando cancelado
                         self.download_thread.start()
-                        self.download_thread.finished.connect(lambda: self.on_download_finished(btn_cancel, btn_download))
+                        self.download_thread.finished.connect(lambda: self.on_download_finished(btn_cancel, btn_download, btn_pause_resume))
                         return
+        else:
+            QMessageBox.warning(self, 'Selection Error', 'Failed to find a download link for selected OS and version.')
 
-        QMessageBox.warning(self, 'Link not found', f'Failed to find a download link for {selected_architecture} {selected_version}.')
+    def update_progress_cancel(self):
+        if self.download_thread:
+            self.download_thread.update_progress_cancel()
+
+    def update_button_state(self, btn_pause_resume, btn_cancel, enable_pause, enable_cancel):
+        btn_pause_resume.setEnabled(enable_pause)
+        btn_cancel.setEnabled(enable_cancel)
+
+    def pause_resume_download(self, btn_pause_resume, btn_cancel):
+        if self.download_thread and self.download_thread.isRunning():
+            self.download_thread.toggle_pause()
+            if self.download_thread.paused:
+                btn_pause_resume.setText('Resume')
+                btn_cancel.setEnabled(False)
+            else:
+                btn_pause_resume.setText('Pause')
+                btn_cancel.setEnabled(True)
 
     def update_download_speed(self, speed, progress_bar):
         current_progress = progress_bar.value()
@@ -417,16 +574,51 @@ class MainWindow(QMainWindow):
         else:
             progress_bar.setFormat(f"Completed.")
 
-    def on_download_finished(self, btn_cancel, btn_download):
+    def on_download_finished(self, btn_cancel, btn_download, btn_pause_resume):
         btn_cancel.setEnabled(False)
+        btn_pause_resume.setEnabled(False)
         btn_download.setEnabled(True)
 
-        QMessageBox.information(self, 'Download Completed', 'Completed.')
+        if self.download_thread and self.download_thread.isFinished():
+            msg_box = QMessageBox()
+            msg_box.setWindowTitle("Notification")
+            msg_box.setText("Download Finished.")
+            msg_box.setIcon(QMessageBox.Information)
+            msg_box.setWindowFlags(Qt.FramelessWindowHint)
+
+            # Estilo CSS para remover a borda onde fica o botão de fechar
+            msg_box.setStyleSheet("""
+                QMessageBox {
+                    background-color: #1e1e1e;
+                    border: none; /* Remove a borda */
+                }
+                QMessageBox QLabel {
+                    color: #dcdcdc;
+                }
+                QMessageBox QPushButton {
+                    background-color: #007bff;
+                    color: white;
+                    padding: 5px;
+                    border-radius: 5px;
+                    min-width: 80px;
+                }
+                QMessageBox QPushButton:hover {
+                    background-color: #0056b3;
+                }
+            """)
+
+            # Adicionando botão OK personalizado
+            ok_button = msg_box.addButton("OK", QMessageBox.AcceptRole)
+            ok_button.setStyleSheet("background-color: #28a745; color: white; padding: 5px; border-radius: 5px; min-width: 80px;")
+            ok_button.clicked.connect(msg_box.accept)
+
+            msg_box.exec_()
 
     def cancel_download(self):
-        if self.download_thread and self.download_thread.isRunning():
-            self.download_thread.terminate()
-            self.download_thread.wait()
+        if self.download_thread:
+            if self.download_thread.isRunning():
+                self.download_thread.terminate()
+                self.download_thread.wait()
 
     def open_in_browser(self, combo_windows, combo_arch, combo_version, download_data):
         selected_index = combo_windows.currentIndex()
